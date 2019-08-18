44ee51da672ec8d4dbc9a47463d23d2470dbb6ad
Wasith Theerapattrathamrong <phai@live.com>
2019-08-19 00:08:47 +0700

    Add Move tests for North

--- swift-marsrovertechchallenge/Direction/NorthTests.swift
+++ swift-marsrovertechchallenge/Direction/NorthTests.swift
@@ -12,4 +12,18 @@ class NorthTests: XCTestCase {
     let actual = North().right
     XCTAssertTrue(actual is East)
   }
+
+  func testMove() {
+    let position = Position(x: 1, y: 2, face: North())
+    let newPosition = North().move(form: position, with: nil)
+    let expectedPosition = Position(x: 1, y: 3, face: North())
+    XCTAssertEqual(expectedPosition, newPosition)
+  }
+
+  func testMoveAndReachMaxY_shouldBeTheSamePositionAsBefore() {
+    let position = Position(x: 1, y: 99, face: North())
+    let maps = Maps(maxX: 88, maxY: 99)
+    let newPosition = North().move(form: position, with: maps)
+    XCTAssertEqual(position, newPosition)
+  }
 }

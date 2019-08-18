22f94cd645293db15a257a3639c8d5b1fe295c36
Wasith Theerapattrathamrong <phai@live.com>
2019-08-19 00:13:15 +0700

    Add test for East

--- swift-marsrovertechchallenge/Direction/EastTests.swift
+++ swift-marsrovertechchallenge/Direction/EastTests.swift
@@ -12,4 +12,18 @@ class EastTests: XCTestCase {
     let actual = East().right
     XCTAssertTrue(actual is South)
   }
+
+  func testMove() {
+    let position = Position(x: 1, y: 2, face: East())
+    let newPosition = East().move(form: position, with: nil)
+    let expectedPosition = Position(x: 2, y: 2, face: East())
+    XCTAssertEqual(expectedPosition, newPosition)
+  }
+
+  func testMoveAndReachMaxX_shouldBeTheSamePositionAsBefore() {
+    let position = Position(x: 88, y: 55, face: East())
+    let maps = Maps(maxX: 88, maxY: 99)
+    let newPosition = East().move(form: position, with: maps)
+    XCTAssertEqual(position, newPosition)
+  }
 }

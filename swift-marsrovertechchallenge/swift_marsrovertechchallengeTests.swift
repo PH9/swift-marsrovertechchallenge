d7895af5e86f30876ef3d6136405234f78b55165
Wasith Theerapattrathamrong <phai@live.com>
2019-08-18 21:07:14 +0700

    Extract variable and named it dummyX and dummyY

--- swift-marsrovertechchallenge/swift_marsrovertechchallengeTests.swift
+++ swift-marsrovertechchallenge/swift_marsrovertechchallengeTests.swift
@@ -2,32 +2,35 @@ import XCTest
 @testable import swift_marsrovertechchallenge
 
 class swift_marsrovertechchallengeTests: XCTestCase {
+
+  let dummyX = 1
+  let dummyY = 2
   
   func test_create_maps() {
     let maps = Maps(maxX: 5, maxY: 5)
   }
   
   func testWhenRoverFaceNorth_afterTurnLeftShouldFaceWest() {
-    let rover = Rover(x: 1, y: 2, face: .north)
+    let rover = Rover(x: dummyX, y: dummyY, face: .north)
     rover.operate(command: .turnLeft)
 
-    let expectedPosition = Position(x: 1, y: 2, face: .west)
+    let expectedPosition = Position(x: dummyX, y: dummyY, face: .west)
     XCTAssertEqual(expectedPosition, rover.position)
   }
   
   func testWhenRoverFaceWest_afterTurnLeftShouldFaceSouth() {
-    let rover = Rover(x: 1, y: 2, face: .west)
+    let rover = Rover(x: dummyX, y: dummyY, face: .west)
     rover.operate(command: .turnLeft)
     
-    let expectedPosition = Position(x: 1, y: 2, face: .south)
+    let expectedPosition = Position(x: dummyX, y: dummyY, face: .south)
     XCTAssertEqual(expectedPosition, rover.position)
   }
   
   func testWhenRoverFaceSouth_afterTurnLeftShouldFaceEast() {
-    let rover = Rover(x: 1, y: 2, face: .south)
+    let rover = Rover(x: dummyX, y: dummyY, face: .south)
     rover.operate(command: .turnLeft)
     
-    let expectedPosition = Position(x: 1, y: 2, face: .east)
+    let expectedPosition = Position(x: dummyX, y: dummyY, face: .east)
     XCTAssertEqual(expectedPosition, rover.position)
   }
 }

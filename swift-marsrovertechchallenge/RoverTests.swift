43c9cd597ead0ad8708259b8f846a81498c0e6f1
Wasith Theerapattrathamrong <phai@live.com>
2019-08-19 00:04:32 +0700

    Add test case for rover operate

--- swift-marsrovertechchallenge/RoverTests.swift
+++ swift-marsrovertechchallenge/RoverTests.swift
@@ -5,6 +5,27 @@ class RoverTests: XCTestCase {
 
   let dummyX = 1
   let dummyY = 2
+
+  class SpyCommand: CommandProtocol {
+
+    private(set) var moveCalledCount = 0
+
+    func move(form position: Position, with maps: Maps?) -> Position {
+      moveCalledCount += 1
+      return Position(x: 8, y: 9, face: South())
+    }
+  }
+
+  func testIsCalledMoveAndSetBack() {
+    let rover = Rover(x: 1, y: 2, face: North())
+    let spy = SpyCommand()
+    rover.operate(command: spy)
+
+    XCTAssertEqual(1, spy.moveCalledCount)
+    XCTAssertEqual(8, rover.position.x)
+    XCTAssertEqual(9, rover.position.y)
+    XCTAssertTrue(rover.position.face is South)
+  }
   
   func testWhenTurnLeftDirectionShouldBeNextCounterclockwise() {
     let rover = Rover(x: dummyX, y: dummyY, face: North())

7020dc3fcc43daae221bbced7df5401b79041a64
Wasith Theerapattrathamrong <phai@live.com>
2019-08-19 01:05:18 +0700

    Create readFile utils

---
+++ swift-marsrovertechchallenge/ReadFileTests.swift
@@ -0,0 +1,18 @@
+import XCTest
+@testable import swift_marsrovertechchallenge
+
+class ReadFileTests: XCTestCase {
+
+  func testReadFile() {
+    let string = try? Utils().readFile(name: "for-test.txt")
+    XCTAssertEqual(
+      """
+5 5
+1 2 N
+LMLMLMLMM
+3 3 E
+MMRMMRMRRM
+
+""", string)
+  }
+}

99d3a2863d213ed0681105066c87a714c25e7555
Wasith Theerapattrathamrong <phai@live.com>
2019-08-19 00:05:09 +0700

    Remove turn test case in rover

--- swift-marsrovertechchallenge/RoverTests.swift
+++ swift-marsrovertechchallenge/RoverTests.swift
@@ -26,46 +26,6 @@ class RoverTests: XCTestCase {
     XCTAssertEqual(9, rover.position.y)
     XCTAssertTrue(rover.position.face is South)
   }
-  
-  func testWhenTurnLeftDirectionShouldBeNextCounterclockwise() {
-    let rover = Rover(x: dummyX, y: dummyY, face: North())
-
-    rover.operate(command: TurnLeft())
-    let expectedFaceWest = Position(x: dummyX, y: dummyY, face: West())
-    XCTAssertEqual(expectedFaceWest, rover.position)
-
-    rover.operate(command: TurnLeft())
-    let expectedFaceSouth = Position(x: dummyX, y: dummyY, face: South())
-    XCTAssertEqual(expectedFaceSouth, rover.position)
-
-    rover.operate(command: TurnLeft())
-    let expectedFaceEast = Position(x: dummyX, y: dummyY, face: East())
-    XCTAssertEqual(expectedFaceEast, rover.position)
-
-    rover.operate(command: TurnLeft())
-    let expectedFaceNorth = Position(x: dummyX, y: dummyY, face: North())
-    XCTAssertEqual(expectedFaceNorth, rover.position)
-  }
-
-  func testWhenTurnRightDirectionShouldBeNextAfterClockwise() {
-    let rover = Rover(x: dummyX, y: dummyY, face: North())
-
-    rover.operate(command: TurnRight())
-    let expectedFaceWest = Position(x: dummyX, y: dummyY, face: East())
-    XCTAssertEqual(expectedFaceWest, rover.position)
-
-    rover.operate(command: TurnRight())
-    let expectedFaceSouth = Position(x: dummyX, y: dummyY, face: South())
-    XCTAssertEqual(expectedFaceSouth, rover.position)
-
-    rover.operate(command: TurnRight())
-    let expectedFaceEast = Position(x: dummyX, y: dummyY, face: West())
-    XCTAssertEqual(expectedFaceEast, rover.position)
-
-    rover.operate(command: TurnRight())
-    let expectedFaceNorth = Position(x: dummyX, y: dummyY, face: North())
-    XCTAssertEqual(expectedFaceNorth, rover.position)
-  }
 
   func testWhenFaceNorthAndMoveShouldBeInNextPosition() {
     let rover = Rover(x: 1, y: 2, face: North())

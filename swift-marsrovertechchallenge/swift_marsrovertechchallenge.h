d32d0a6de6850d0a3f33d63a23b56165923b903d
Wasith Theerapattrathamrong <phai@live.com>
2019-08-18 20:37:44 +0700

    Project init

---
+++ swift-marsrovertechchallenge/swift_marsrovertechchallenge.h
@@ -0,0 +1,11 @@
+#import <UIKit/UIKit.h>
+
+//! Project version number for swift_marsrovertechchallenge.
+FOUNDATION_EXPORT double swift_marsrovertechchallengeVersionNumber;
+
+//! Project version string for swift_marsrovertechchallenge.
+FOUNDATION_EXPORT const unsigned char swift_marsrovertechchallengeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <swift_marsrovertechchallenge/PublicHeader.h>
+
+
---
+++ swift-marsrovertechchallenge/swift_marsrovertechchallengeTests.swift
@@ -0,0 +1,9 @@
+import XCTest
+@testable import swift_marsrovertechchallenge
+
+class swift_marsrovertechchallengeTests: XCTestCase {
+  
+  func test_() {
+    // DUMMY TEST OR IT FAIL
+  }
+}

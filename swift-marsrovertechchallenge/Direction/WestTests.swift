7743d48ca4773b8589eba77ef0948b7e8cee0ceb
Wasith Theerapattrathamrong <phai@live.com>
2019-08-19 00:10:58 +0700

    Add test for West

--- swift-marsrovertechchallenge/Direction/WestTests.swift
+++ swift-marsrovertechchallenge/Direction/WestTests.swift
@@ -12,4 +12,18 @@ class WestTests: XCTestCase {
     let actual = West().right
     XCTAssertTrue(actual is North)
   }
+
+  func testMove() {
+    let position = Position(x: 1, y: 2, face: West())
+    let newPosition = West().move(form: position, with: nil)
+    let expectedPosition = Position(x: 0, y: 2, face: West())
+    XCTAssertEqual(expectedPosition, newPosition)
+  }
+
+  func testMoveAndReachMaxX_shouldBeTheSamePositionAsBefore() {
+    let position = Position(x: 0, y: 55, face: West())
+    let maps = Maps(maxX: 88, maxY: 99)
+    let newPosition = West().move(form: position, with: maps)
+    XCTAssertEqual(position, newPosition)
+  }
 }

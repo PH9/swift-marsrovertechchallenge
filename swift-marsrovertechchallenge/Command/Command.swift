865a570454e5fb77b4e759719fd3c5612101f582
Wasith Theerapattrathamrong <phai@live.com>
2019-08-21 13:45:14 +0700

    Extract commandFactory

--- swift-marsrovertechchallenge/Command/Command.swift
+++ swift-marsrovertechchallenge/Command/Command.swift
@@ -1,33 +1,3 @@
-enum CommandFactoryError: Error {
-  case cannotCreateBundle
-  case classNotFound(for: String)
-  case classIsNotNSObject(for: String)
-  case classDoesNotConformToCommandProtocol(for: String)
-}
-
-class CommandFactory {
-
-  func create(rawValue: String) throws -> CommandProtocol {
-    guard let namespace = Bundle(for: type(of: self)).infoDictionary?["CFBundleExecutable"] as? String else {
-      throw CommandFactoryError.cannotCreateBundle
-    }
-
-    guard let anyClassType = NSClassFromString("\(namespace).\(rawValue)") else {
-      throw CommandFactoryError.classNotFound(for: rawValue)
-    }
-
-    guard let objectType: NSObject.Type = anyClassType as? NSObject.Type else {
-      throw CommandFactoryError.classIsNotNSObject(for: rawValue)
-    }
-
-    guard let command = objectType.init() as? CommandProtocol else {
-      throw CommandFactoryError.classDoesNotConformToCommandProtocol(for: rawValue)
-    }
-
-    return command
-  }
-}
-
 class TurnLeft: NSObject, CommandProtocol {
 
   func move(from position: Position, with maps: Maps?) -> Position {
@@ -36,7 +6,7 @@ class TurnLeft: NSObject, CommandProtocol {
 }
 
 class L: TurnLeft {
-  
+
 }
 
 class TurnRight: NSObject, CommandProtocol {
@@ -59,5 +29,5 @@ class Move: NSObject, CommandProtocol {
 }
 
 class M: Move {
-  
+
 }
